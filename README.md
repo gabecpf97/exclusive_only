187140c2c62d0f7f6b6ac8cad4ac5abafb0b4930
Gabriel <gabrielcpf2997@gmail.com>
2021-12-19 16:19:31 -0800

    first commit basic files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Exclusive club app
+This is simple web page that allow a log in user to write post and see 
+who the other post's authors are, while user that are not loged in can only
+see the post.
+
+## This project uses
+node
+express
+passport
---
+++ app.js
@@ -0,0 +1,53 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+const mongoose = require('mongoose');
+const mongoDB = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.scu29.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(compression());
+app.use(helmet());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
