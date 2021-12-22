ce02f4dfba5dbfdde38f18bf77ffafc400f9befa
Gabriel <gabrielcpf2997@gmail.com>
2021-12-21 16:14:10 -0800

    added delete and update post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 const Post = require('../models/post');
 const Media = require('../models/media');
-const { body, validationResult } = require('express-validator');
+const { body, validationResult, check } = require('express-validator');
 const async = require('async');
 const fs = require('fs');
 const path = require('path');
@@ -35,3 +35,132 @@ exports.post_detail = (req, res, next) => {
     });
 }
 
+exports.post_create_get = (req, res, next) => {
+    res.render('index', {title: 'Create Post', page: './post_form', 
+                        content: {title: 'Create Post'}});
+}
+
+exports.post_create_post = [
+    body('post_title', 'Title must not be empty.').trim().isLength({min: 1}).escape(),
+    body('message').trim().escape(),
+    check('media', 'Please upload an image file').custom((value, {req}) => {
+        return req.file.mimetype.substring(0, req.file.mimetype.indexOf('/')) === 'image';
+    }),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        async.parallel({
+            post: (callback) => {
+                callback(null, new Post({
+                    date: new Date,
+                    title: req.body.post_title,
+                    message: req.body.message,
+                    user: res.locals.currentUser.id,
+                }));
+            },
+            media: (callback) => {
+                const media = new Media({
+                    data: fs.readFileSync(path.join(__dirname, '../uploads/', req.file.filename)),
+                    content_type: 'image/jpg',
+                });
+                media.save(err => {
+                    if (err)
+                        return next(err);
+                    callback(null, media);
+                });
+            },
+        }, (err, results) => {
+            if (err)
+                return next(err);
+            const post = results.post; 
+            post.media = results.media;
+            if (!errors.isEmpty()) {
+                res.render('index', {title: 'Create Post', page: './post_form', 
+                                    content: {
+                                        title: 'Create Post',
+                                        post: post,
+                                        errors: errors.array()
+                                    }});
+                return;
+            } else {
+                post.save(err => {
+                    if (err)
+                        return next(err);
+                    res.redirect(post.url);
+                });
+            }
+        });
+    }
+]
+
+exports.post_delete_get = (req, res, next) => {
+    Post.findById(req.params.id).populate('user').populate('media').exec((err, thePost) => {
+        if (err)
+            return next(err);
+        if (thePost === null) {
+            const err = new Error('No such post');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('index', {title: 'Delete post', page: './post_delete', 
+                            content: {post: thePost}});
+    });
+}
+
+exports.post_delete_post = (req, res, next) => {
+    Post.findById(req.params.id).exec((err, thePost) => {
+        if (err)
+            return next(err);
+        Post.findByIdAndRemove(req.body.postid, (err) => {
+            if (err)
+                return next(err);
+            res.redirect('/');
+        });
+    });
+}
+
+exports.post_update_get = (req, res, next) => {
+    Post.findById(req.params.id).exec((err, thePost) => {
+        if (err)
+            return next(err);
+        if(thePost === null) {
+            const err = new Error('No such post');
+            err.status = 404;
+            return next(err);
+        }
+        res.render('index', {title: 'Update Post', page: './post_form', 
+                            content: {
+                                title: 'Update Post',
+                                post: thePost,
+                            }});
+    });
+}
+
+exports.post_update_post = [
+    body('post_title', 'Title must not be empty.').trim().isLength({min: 1}).escape(),
+    body('message').trim().escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        const post = new Post({
+            date: req.body.date,
+            title: req.body.post_titel,
+            message: req.body.message,
+            media: req.body.media,
+            user: res.locals.currentUser.id,
+            _id: req.params.id
+        });
+        if (!errors.isEmpty()) {
+            res.render('index', {title: 'Update Post', page: './psot_form',
+                                content: {
+                                    title: 'Update Post',
+                                    post: post
+                                }});
+            return;
+        } else {
+            Post.findByIdAndUpdate(req.params.id, post, {}, (err, thePost) => {
+                if(err)
+                    return next(err);
+                res.redirect(thePost.url);
+            });
+        }
+    }
+]
--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,19 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 const userController = require('../controllers/userController');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' ,
-    fileFilter: (req, file, cb) => {
-        if (file.mimetype.substring(0, file.mimetype.indexOf('/')) === 'image') {
-            cb(null, true);
-        } else {
-            cb(null, false);
-            return cb(new Error('Please only upload images file, Click back'));
-        }
-}});
+const upload = multer({dest: 'uploads/'});
 
 /* GET home page. */
 router.get('/', postController.index);
 
 /* GET and POST for Posts */
-// router.get('/post/create', upload.single('media'), postController.post_create_get);
-// router.post('/post/create', postController.post_create_post);
+router.get('/post/create', postController.post_create_get);
+router.post('/post/create', upload.single('media'), postController.post_create_post);
 router.get('/post/:id', postController.post_detail);
-// router.get('post/:id/delete', postController.post_delete_get);
-// router.post('post/:id/delete', postController.post_delete_post);
-// router.get('/post/:id/update', upload.single('media'), postController.post_update_get);
-// router.post('/post/:id/update', postController.post_update_post);
+router.get('/post/:id/delete', postController.post_delete_get);
+router.post('/post/:id/delete', postController.post_delete_post);
+router.get('/post/:id/update', postController.post_update_get);
+router.post('/post/:id/update', upload.single('media'), postController.post_update_post);
 
 // /* GET and POST for Users */
 router.get('/user/account', userController.user_account_get);
